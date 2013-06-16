8782d9588f0e5b1c0f6c1ff777f888d38cc3ba32
Mattijs Ugen <mattijsugen@gmail.com>
2013-06-16 15:04:17 +0200

    Introduce Ingredients and Ingredient models
    Allows for the use of separate ingredient lists for parts of a recipe
    (like the sauce on a pizza or whathaveyou). Ingredient is split up in
    qunatity, unit and name to allow for future automatic multiplication
    of quantities for a requested number of people.
    
    The current admin is hard to use as it involves the creation of many
    intermediate models to make up the ingredient listings for a single
    recipe. That should change in the future :P
    
    Heavily inspired by @ekeijl's approach in PR #4.
    
    This commit also goes towards #3.

--- keuken/admin.py
+++ keuken/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Recipe
+from .models import Ingredient, Ingredients, Recipe
 
+admin.site.register(Ingredient)
+admin.site.register(Ingredients)
 admin.site.register(Recipe)
-
--- keuken/models.py
+++ keuken/models.py
@@ -1,15 +1,32 @@
-from django.core.urlresolvers import reverse
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Ingredient(models.Model):
+    quantity = models.DecimalField(decimal_places=2, max_digits=6)
+    unit = models.CharField(max_length=32, blank=True)
+    name = models.CharField(max_length=32, blank=False)
+    origin = models.ForeignKey('Ingredients')
+
+    def __str__(self):
+        return '{}{} {}'.format(self.quantity, self.unit, self.name)
+
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=256, blank=True)
+    note = models.TextField(blank=True)
+    recipe = models.ForeignKey('Recipe', blank=False)
+
+    def __str__(self):
+        return '{} ({})'.format(self.name or 'Ingredients', self.recipe)
+
+
 class Recipe(models.Model):
-    name = models.CharField(max_length = 256, blank = False)
+    name = models.CharField(max_length=256, blank=False)
     author = models.ForeignKey(User)
-    pub_date = models.DateField(auto_now_add = True)
-    # a free form text field containing the ingredient list
-    ingredients = models.TextField(blank = False)
+    pub_date = models.DateField(auto_now_add=True)
     # a free form text field containing the cooking directions
-    directions = models.TextField(blank = False)
+    directions = models.TextField(blank=False)
 
     def __str__(self):
         return self.name
