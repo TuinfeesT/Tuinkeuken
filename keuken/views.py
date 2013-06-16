7c2cbabadd7961a9b9c572afad1390f7d17ebfd1
Mattijs Ugen <mattijsugen@gmail.com>
2013-06-16 14:17:05 +0200

    Move all templates to project
    Have app views refer to templates the using project is required to provide
    (approach taken from django-registration). Be sure to use namespaces
    for the templates involved.

--- keuken/views.py
+++ keuken/views.py
@@ -5,10 +5,9 @@ from django.shortcuts import get_object_or_404, render_to_response
 from .models import Recipe
 
 def details(request, id):
-    return render_to_response('recipe-details.html', {
+    return render_to_response('keuken/recipe.html', {
         'recipe': get_object_or_404(Recipe, pk=id)
     })
 
 def list_by_author(request, author):
     return HttpResponse(get_object_or_404(User, username=author))
-
