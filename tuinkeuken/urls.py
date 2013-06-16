a3c9012e955af501b8c8ea8b17500195896619b2
Mattijs Ugen <mattijsugen@gmail.com>
2013-06-16 15:06:49 +0200

    Clean up comments in urls

--- tuinkeuken/urls.py
+++ tuinkeuken/urls.py
@@ -4,10 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'tuinkeuken.views.index', name='index'),
     url(r'^recipes/', include('keuken.urls', namespace='keuken')),
 
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
